85743f10f6baf0b43853401328fd2e561116ccf1
Parksangwoo <psu8430@gmail.com>
2022-06-18 17:42:51 +0900

    Fix: 파일 제거에 따른 코드라인 수정

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store.js'
-createApp(App).use(store).mount('#app')
+import { createApp } from 'vue';
+import App from './App.vue';
+
+createApp(App).mount('#app');
